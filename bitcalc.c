47d41e2cff484cbb20eb81d5fbafd3b35993bdfe
worthless443 <aissypewdiepie@gmail.com>
2023-05-26 08:55:30 +0530

    first: heavily non-working code

---
+++ bitcalc.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stddef.h>
+
+struct Store {
+	int start,mskdout,start_bit,mask_bit;
+	int  mask_bit_prev;
+	int value;
+};
+
+void get_last_digit(struct Store *store, int _start) { 
+	store->start = _start;
+	int w = 0,w1 = 0, start = _start;
+	int tmp2 = 1;
+	while(start) {
+		int tmp = start, i_c = 0, e = 1;
+		while(tmp!=1) {
+			e = (e << 1) + 1;
+			tmp>>=1;
+		}
+		e <<= 1;
+		// this part is bad
+		if(((start ^ e) - start) != 2) w1 = w;
+		++w;
+		start>>=1;
+	}
+	store->start_bit = w;
+	store->mask_bit = w1;
+	for(int i=1;i<w1;++i)
+		tmp2=(tmp2<<1) + 1;
+	store->mask_bit_prev = tmp2;
+	// this part is messy too 	
+	tmp2 &= _start;
+	store->mskdout = tmp2;
+	tmp2 >>= 3;
+	tmp2 |= 1;
+	store->value = tmp2;
+}
+
+int main(int argc, char **argv) {
+	int start = argc < 2 ? 355 : atoi(argv[1]);
+	struct Store store;
+	get_last_digit(&store,start);
+	printf("%d\n", store.value);
+	return 0;
+}
